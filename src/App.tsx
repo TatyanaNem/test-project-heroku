67d6877baddea74e099adc0508c34b192b38c590
Tatyana Nemchina <23angorka07@gmail.com>
2022-10-29 20:11:46 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from './components/Button';
+
+type ShowType = {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false
+}
 
 function App() {
+  let [show, setShow] = useState<ShowType[]>([])
+
+  const useShowUp = () => {
+          fetch('https://jsonplaceholder.typicode.com/todos')
+              .then(response => response.json())
+              .then(json => setShow(json))
+  }
+
+  const deleteAll = () => {
+      setShow([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button name={'Show me'} callback={useShowUp} />
+        <Button name={'Delete all'} callback={deleteAll} />
+      <ul>
+          {show.map(el => {
+              return (
+                  <li key={el.id}>
+                      <span>{el.userId}</span>
+                      <span>{el.title}</span>
+                      <input onChange={() => {}} type='checkbox' checked={el.completed}/>
+                  </li>
+              )
+          })}
+      </ul>
     </div>
   );
 }
